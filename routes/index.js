081c1fe3f3499bc10a98d3cd89895b41e7e17cdf
Jason Hwee <1216418+hweej@users.noreply.github.com>
2025-06-03 09:06:10 -0400

    Render summary counts via parameters

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,28 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "SCRP" });
+  // const module_counts = {
+  //   tcm: {
+  //     cells: '3000',
+  //     patients: '30',
+  //     samples: '10',
+  //     organs: '5',
+  //     cancers: '1',
+  //   }
+  // };
+  // process.env.MODULES.split(',').forEach((arg) => {
+  //   // Sum modules
+  //   console.log("Module: " + arg);
+  // });
+  const counts = {
+    cells: 656742,
+    patients: 670,
+    samples: 401,
+    organs: 21,
+    cancers: 16,
+    studies: 5
+  };
+  res.render("index", { title: "SCRP", counts: counts });
 });
-// router.get("/index.html", function (req, res, next) {
-//   res.render("index", { title: "Express" });
-// });
 
 module.exports = router;
