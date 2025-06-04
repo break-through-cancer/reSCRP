2b4250fe99fa429010489a1022eb68e0e46bb908
Jason Hwee <1216418+hweej@users.noreply.github.com>
2025-06-04 09:17:07 -0400

    Dynamically sum the declared study modules

--- routes/index.js
+++ routes/index.js
@@ -1,30 +1,41 @@
 var express = require("express");
 var router = express.Router();
+var counts = require("../module_counts");
+
+// Move to utils
+function sumObjectsByKey(objs) {
+  return objs.reduce((a, b) => {
+    for (let k in b) {
+      if (b.hasOwnProperty(k))
+        a[k] = (a[k] || 0) + b[k];
+    }
+    return a;
+  }, {});
+};
+
+// Using the declared modules, filter out module counts
+var objs = [];
+process.env.MODULES.split(',').forEach((arg) => {
+  if (arg in counts) {
+    objs.push(counts[arg]);
+  };
+});
+
+let total_count = sumObjectsByKey(objs);
+total_count["studies"] = objs.length;
+
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  // const module_counts = {
-  //   tcm: {
-  //     cells: '3000',
-  //     patients: '30',
-  //     samples: '10',
-  //     organs: '5',
-  //     cancers: '1',
-  //   }
+  // const counts = {
+  //   cells: 656742,
+  //   patients: 670,
+  //   samples: 401,
+  //   organs: 21,
+  //   cancers: 16,
+  //   studies: 5
   // };
-  // process.env.MODULES.split(',').forEach((arg) => {
-  //   // Sum modules
-  //   console.log("Module: " + arg);
-  // });
-  const counts = {
-    cells: 656742,
-    patients: 670,
-    samples: 401,
-    organs: 21,
-    cancers: 16,
-    studies: 5
-  };
-  res.render("index", { title: "SCRP", counts: counts });
+  res.render("index", { title: "SCRP", counts: total_count });
 });
 
 module.exports = router;
